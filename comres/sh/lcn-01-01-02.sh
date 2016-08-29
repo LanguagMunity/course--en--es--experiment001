ebd0739a594bdcfae9b2291c9181198ce935262e
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-08-29 03:27:17 -0400

    Began new command for invoking lessons - and entered much more data

---
+++ comres/sh/lcn-01-01-02.sh
@@ -0,0 +1,11 @@
+
+
+echo
+echo
+echo
+
+languamunity agri -ft "${LANGDRILL_X_SCRAT}/x.json" -f "${LANGDRILL_X_LCDIR}/01-01-tx.json" -lm 30
+
+languamunity lc-take "${LANGDRILL_X_SCRAT}/x.json"
+languamunity lc-take "${LANGDRILL_X_LCDIR}/01-02-tx.json"
+
---
+++ comres/sh/lcn-new.sh
@@ -0,0 +1,9 @@
+
+
+echo
+echo
+echo
+
+
+languamunity lc-take "${LANGDRILL_X_LCDIR}/01-03-tx.json"
+
